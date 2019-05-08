98550969b4bfd3c277ec48cb802519433f70dc25
Thomas <thomashill1993@me.com>
2019-05-08 18:51:36 -0500

    Working

---
+++ FirstVR/Source/FirstVR/VRCharacter.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Camera/CameraComponent.h"
+#include "Components/InputComponent.h"
+#include "Components/SceneCaptureComponent.h"
+#include "VRCharacter.generated.h"
+
+
+UCLASS()
+class FIRSTVR_API AVRCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AVRCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	UCameraComponent*  m_camera;
+	USceneComponent* m_VRRoot;
+	FVector m_previousPos;
+	void MoveForward(float throttle);
+	void MoveRight(float throttle);
+
+};
+
