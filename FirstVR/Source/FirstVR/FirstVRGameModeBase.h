f8fa90a6f572fdfadc507336a35cc4276c7eab95
Thomas <thomashill1993@me.com>
2019-05-08 16:39:46 -0500

    Fire is Cleansing ;D

---
+++ FirstVR/Source/FirstVR/FirstVRGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FirstVRGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIRSTVR_API AFirstVRGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
