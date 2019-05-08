a9589ceb3d3829fb6b5bc4965e6ba338c9d0dafe
Thomas <thomashill1993@me.com>
2019-05-07 22:11:19 -0500

    Added movement with WASD

---
+++ ArchitectureExplorer/Source/ArchitectureExplorer/VRCharacter.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "VRCharacter.h"
+
+
+// Sets default values
+AVRCharacter::AVRCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	m_camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	m_camera->SetupAttachment(GetRootComponent());
+
+}
+
+// Called when the game starts or when spawned
+void AVRCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AVRCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AVRCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAxis(TEXT("Forward"), this, &AVRCharacter::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("Right"), this, &AVRCharacter::MoveRight);
+}
+
+void AVRCharacter::MoveForward(float throttle)
+{
+	AddMovementInput(throttle* m_camera->GetForwardVector());
+}
+
+void AVRCharacter::MoveRight(float throttle)
+{
+	AddMovementInput(throttle* m_camera->GetRightVector());
+}
+
